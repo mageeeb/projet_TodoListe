4d872171406de1e160f2d874e90355d37a2823c7
julia-dm <yulia.2508dmitruk@gmail.com>
2026-08-27 16:24:04 +0200

    Add task

---
+++ Yuliia-Laetitia/js/script.js
@@ -0,0 +1,18 @@
+const form=document.getElementById("todoForm");
+const input=document.getElementById("todoInput");
+const list=document.getElementById("todoList");
+const count=document.getElementById("count");
+
+
+
+
+function addTask(e){
+e.preventDefault();
+	let task=document.createElement("p");
+	console.log(task);	
+	task.textContent=input.value;
+	list.appendChild(task);
+	input.textContent=" "
+
+}
+form.addEventListener("submit",addTask);
